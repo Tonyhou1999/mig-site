165a93f2379cbd825058099cf9312d80fa935663
John Marangola <john.marangola@gmail.com>
2022-08-22 23:42:35 -0400

    add navbar component, home page layout animation, homepage logo, title,hamburger icon for resize

--- components/Nav/NavBar.jsx
+++ components/Nav/NavBar.jsx
@@ -1,55 +1,47 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import styles from './NavBar.module.css';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
-import "./NavBar.module.css";
+import styles from "./NavBar.module.css";
 import { memo } from "react";
-
-const NavBar = memo(
-  ({
-    navBarArray = [{ text: "people", text: "people2" }],
-    navBarStyle,
-    endAlign,
-  }) => {
-  navBarStyle = navBarStyle || "defaultNavBarStyle";
+import Link from 'next/link';
 
 
+const NavBar = memo(({ navBarArray = [] }) => {
+  if (!navBarArray || !navBarArray.length) {
+    return null;
+  } 
+  // TODO: add modular alignment
+  // TODO: add mig logo to navbar
   return (
-    <body style={{"height": "20000px"}}>
-      <Navbar bg="light" expand="lg" className="navbar fixed-top navbar-expand-sm navbar-light">
-        <div className="container">
-        <Navbar.Brand href="#">
+    <Navbar bg="white" expand="lg" className="navbar fixed-top navbar-expand-sm navbar-light">
+      <div className="container">
+        <Navbar.Brand href="#" className={styles.link}> {/* TODO: replace */}
           MIG
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="n-navbar-nav" />
-        <Navbar.Collapse id="n-navbar-nav">
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
           <Nav className="ms-auto">
-            <NavLink text={"About"} />
-            <NavLink text={"Contact"} />
-
+            <NavElements navBarArray={navBarArray}/>
           </Nav>
         </Navbar.Collapse>
-        </div>
+      </div>
     </Navbar>
-    </body>
-
-);
-
-
+  );
 });
 
 const NavElements = ({ navBarArray }) => {
   return navBarArray.map((element) => {
     const {
       disabled = false,
-      linkDest = null,
+      linkDest = "/",
       isDropDown = false,
       dropDownMembers = null,
       text,
     } = element;
 
+    // TODO: add dropdown
     return isDropDown && dropDownMembers ? 
       null :
         <NavLink text={text} 
@@ -59,35 +51,23 @@ const NavElements = ({ navBarArray }) => {
   });
 };
 
-/**
- * 
- * @param {string} text Displayed text 
- * @param {string=} linkDest Optional. Link destination
- * @param {bool=} disabled Optional. If true, the link is disabled.
- * @returns a NavLink component 
- */
 const NavLink = ({ text, linkDest, disabled }) => {
   // Check for invalid text string 
   if (!text || typeof text != "string" || text.length == 0) {
     console.error("[NavLink] Error: text must be a valid string");
     return null;
   }
-  // Check for disabled link
-  if (disabled) {
-    linkDest = "";
-  } else {
-    linkDest = linkDest || "";
-  }
-  return (
-    <Nav.Item>
-      <Nav.Link ahref={linkDest} className={styles.navlinkText}> {text} </Nav.Link>
-    </Nav.Item>
+  return (disabled ? 
+    <Link href={linkDest} passHref>
+      <Nav.Link className={styles.link} disabled>{text}</Nav.Link>
+    </Link> : 
+    <Link href={linkDest} passHref>
+      <Nav.Link className={styles.link}>{text}</Nav.Link>
+    </Link> 
   );
 };
 
-const NavDropDown = () => {
-
-};
-
+// TODO: 
+const NavDropDown = () => null;
 
 export default NavBar;
--- pages/people/index.js
+++ pages/people/index.js
@@ -1,7 +1,4 @@
-import {NavBar} from "../../components/Nav";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 // @TODO: impl.
 export default function People() {
-  return <NavBar props={{}} props2={[]}/>
+  return <div> <h1> PEOPLE </h1></div>;
 };
